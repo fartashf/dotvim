681b6aafb663bdc8899044c9c38f5179af13bc25
Fartash Faghri <fartash.faghri@gmail.com>
2015-09-24 13:22:51 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ pip install --user flake8 pep8 autopep8 rope
 sudo apt-get install ack ruby-dev
 pushd bundle/ropevim
 python setup.py install --user
-export PATH="$PATH:~/.local/bin/" >> ~/.bashrc
+echo 'export PATH="$PATH:~/.local/bin/"' >> ~/.bashrc
